5ea86aee296a9a15da51b20c1253e8d5eda293ea
David Alderson <dsa027@gmail.com>
2017-12-22 13:32:14 -0700

    add Landing and Library page routes and nav

---
+++ src/components/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <section className='landing'>
+      {'Landing page will go here'}
+    </section>
+  )
+}
+
+export default Landing;
---
+++ src/components/Library.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Library = () => {
+  return (
+    <section className='library'>
+      {'This is the library page'}
+    </section>
+  )
+}
+
+
+export default Library
