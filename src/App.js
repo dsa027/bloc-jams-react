dc3db6ab537b4afd0494e06a0048025a080df6dc
David Alderson <dsa027@gmail.com>
2017-12-22 14:02:06 -0700

    Add the Album component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Link } from 'react-router-dom';
 import './App.css';
 import Landing from './components/Landing';
 import Library from './components/Library';
+import Album from './components/Album';
 
 class App extends Component {
   render() {
@@ -11,14 +12,16 @@ class App extends Component {
         <header>
           <nav>
             <p>
-              <Link to='/' style={{'margin-right': '20px'}}>Landing</Link>
-              <Link to='/library'>Library</Link>
+              <Link className='link' to='/'>Landing</Link>
+              <Link className='link' to='/library'>Library</Link>
+              <Link className='link' to='/album'>Album</Link>
             </p>
           </nav>
         </header>
         <main>
           <Route exact path='/' component={Landing} />
           <Route path='/library' component={Library} />
+          <Route path='/album' component={Album} />
         </main>
       </div>
     );
---
+++ src/components/Album.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Album = () => {
+  return (
+    <section className='album'>
+      {'Here\'s that album you\'ve been looking for!'}
+    </section>
+  )
+}
+
+export default Album;
