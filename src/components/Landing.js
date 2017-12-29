4d600c97002ec8dfc02b04fc84e586c3bffc17b3
David Alderson <dsa027@gmail.com>
2017-12-28 17:01:32 -0700

    Style landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,26 +1,13 @@
 import React from 'react';
-// import './landing.css'
+import './landing.css'
 
 const Landing = () => {
   return (
     <section className='landing'>
-      <h1 className="hero-title">Turn the music up!</h1>
-
-      <section className="selling-points">
-        <div className="point">
-          <h2 className="point-title">Choose your music</h2>
-          <p className="point-description">The world is full of music; why should you have to listen to music that someone else chose?</p>
-        </div>
-        <div className="point">
-          <h2 className="point-title">Unlimited, streaming, ad-free</h2>
-          <p className="point-description">No arbitrary limits. No distractions.</p>
-        </div>
-        <div className="point">
-          <h2 className="point-title">Mobile enabled</h2>
-          <p className="point-description">Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
-        </div>
+      <p><img src='/assets/images/WaveStreamerLogo.png'/></p>
+      <p><span className='hugetext'>Turn the music up!</span></p>
+      <p id='paragraph'><span className='smalltext'>Get the</span> <span className='bigtext'>first streamer</span> <span className='smalltext'>that has </span><span className='bigtext'>unlimited</span> <span className='smalltext'>streaming and is</span> <span className='bigtext'>ad-free</span>. <span className='smalltext'>There are </span><span className='bigtext'>no limits</span><span className='smalltext'>. There are </span><span className='bigtext'>no distractions</span><span className='smalltext'>. Listen at your desk or</span> <span className='bigtext'>on the go</span><span className='smalltext'>. Wave Streamr is available on </span><span className='bigtext'>all platforms</span><span className='smalltext'>.</span></p>
       </section>
-    </section>
   )
 }
 
